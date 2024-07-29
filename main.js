0259d393c87df86694f39141340626572b523a93
Magician774 <hodjicsuper22@gmail.com>
2024-07-29 14:55:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let a = prompt ("First Number")
+let b = prompt ("Second Number")
+let c = prompt ("Sign")
+if ( c =  ' +') {
+console.log( a + b );
+}if ( c = " -") {
+console.log(a - b );
+}if ( c = " /") {
+console.log(a / b);
+}if ( c = "* ") {
+console.log( a * b);
+}
+else {
+console.log("Error");
+}
